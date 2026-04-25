ad7b16964b8410c156914fbda73728380d16b960
Guilherme Soares <guisxares@hotmail.com>
2026-04-25 17:53:10 -0300

    feat: adicionado rota da página de autenticação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthPage } from "./pages/auth/Index";
 import { IndexPage } from "./pages/index/Index";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/index" element={<IndexPage />} />
       </Routes>
     </BrowserRouter>
