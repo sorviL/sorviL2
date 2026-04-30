7a9af4897af20c1d1f185615b12808f667f3a7e5
Guilherme Soares <guisxares@hotmail.com>
2026-04-30 20:41:39 -0300

    feat: adicionado card component

---
+++ frontend/src/components/auth/card/AuthCard.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import "./AuthCard.scss";
+
+type AuthCardProps = {
+  title: string;
+  subtitle: string;
+  children: ReactNode;
+};
+
+export function AuthCard({ title, subtitle, children }: AuthCardProps) {
+  return (
+    <div className="auth-card">
+      <div className="auth-card-header">
+        <h2 className="auth-card-title">{title}</h2>
+        <p className="auth-card-subtitle">{subtitle}</p>
+      </div>
+      <div className="auth-card-body">{children}</div>
+    </div>
+  );
+}
