c2f6ea1e39559874df330242d85e70ec9802f1c6
diegossz <diegosato2208@gmail.com>
2026-04-30 21:04:48 -0300

    feat(app) adicionada rota para pagina de book

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthPage } from "./pages/auth/Index";
 import { IndexPage } from "./pages/index/Index";
 import { BookshelfPage } from "./pages/bookshelf/Bookshelf";
+import { BookPage } from "./pages/book/BookPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/bookshelf" element={<BookshelfPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+return (
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<IndexPage />} />
+<Route path="/auth" element={<AuthPage />} />
+<Route path="/bookshelf" element={<BookshelfPage />} />
+<Route path="/book/:bookId" element={<BookPage />} />
+</Routes>
+</BrowserRouter>
+);
 }
 
 export default App;
