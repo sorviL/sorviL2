05ef21fda3529012587c3203c7e53f74d89df457
diegossz <diegosato2208@gmail.com>
2026-04-30 21:10:37 -0300

    feat(styles): Refatorado as cores do ShelfStatus para utilizar variáveis CSS, melhorando a consistência e a manutenção do tema

--- frontend/src/types/bookshelf.ts
+++ frontend/src/types/bookshelf.ts
@@ -9,11 +9,11 @@ export const ShelfStatus = {
 export type ShelfStatus = (typeof ShelfStatus)[keyof typeof ShelfStatus];
 
 export const ShelfStatusBadgeColor: Record<ShelfStatus, string> = {
-    [ShelfStatus.Reading]: "#bfb039",
-    [ShelfStatus.WantToRead]: "#6337ad",
-    [ShelfStatus.Read]: "#25a152",
-    [ShelfStatus.Rereading]: "#43a2d1",
-    [ShelfStatus.Abandoned]: "#912511"
+    [ShelfStatus.Reading]: "var(--color-status-reading)",
+    [ShelfStatus.WantToRead]: "var(--color-status-want-to-read)",
+    [ShelfStatus.Read]: "var(--color-status-read)",
+    [ShelfStatus.Rereading]: "var(--color-status-rereading)",
+    [ShelfStatus.Abandoned]: "var(--color-status-abandoned)"
 };
 
 export const SHELF_STATUS_LABEL: Record<ShelfStatus, string> = {
