010af4c0acdaee51e8571a45a53912d09935c403
Ágatha Corrêa <agathacduarte@gmail.com>
2026-05-03 04:28:10 -0300

    feat(split-text): adicionar imports e registrar plugins GSAP

---
+++ frontend/src/components/splitText/SplitText.tsx
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect, useState } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText as GSAPSplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, GSAPSplitText, useGSAP);
+
+export default function SplitText() {
+	return null;
+}
