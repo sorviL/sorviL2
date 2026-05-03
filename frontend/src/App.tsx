6f7a578e306b7090fa217a69856be06a2e2b9f7b
diegossz <diegosato2208@gmail.com>
2026-05-03 01:35:35 -0300

    feat(reviews): adicionada rota para mock reviews page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { ProtectedRoute } from "./components/auth/ProtectedRoute";
 import { Navbar } from "./components/navbar/Index";
 import { Footer } from "./components/footer/Footer";
 import { BookPage } from "./pages/book/BookPage";
+import { ReviewsMockPage } from "./pages/reviewsMock/ReviewsMock";
 
 function AppShell() {
     const location = useLocation();
@@ -18,12 +19,14 @@ function AppShell() {
             {!isAuthPage && <Navbar />}
             <main className="app-main">
                 <Routes>
+                    <Route path="/mock-reviews" element={<ReviewsMockPage />} />
                     <Route path="/auth" element={<AuthPage />} />
                     <Route element={<ProtectedRoute />}>
                         <Route path="/" element={<IndexPage />} />
                         <Route path="/index" element={<IndexPage />} />
                         <Route path="/bookshelf" element={<BookshelfPage />} />
                         <Route path="/book/:id" element={<BookPage />} />
+                        
                     </Route>
                 </Routes>
             </main>
