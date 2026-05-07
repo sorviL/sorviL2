7aadab47a91228f7583847891cacb5eafdb9ffa5
Ágatha Corrêa <agathacduarte@gmail.com>
2026-05-07 17:47:56 -0300

    Revert "fix(frontend): corrigir erros de build TypeScript"
    This reverts commit 9527a66dde27cfb4b083fc222e882fa3f03aadd8.

--- frontend/src/components/profile/ProfileRecentReviews.tsx
+++ frontend/src/components/profile/ProfileRecentReviews.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState, type ReactElement } from "react";
+import { useEffect, useState } from "react";
 import "../../assets/css/profile/profile-recent-reviews.scss";
 import { getRecentProfileReviews, type RecentProfileReview } from "../../services/profile.service";
 
@@ -32,7 +32,7 @@ function renderStars(rating: number) {
     const hasHalf = (clamped - fullStars) >= 0.5;
     const emptyStars = 5 - fullStars - (hasHalf ? 1 : 0);
 
-    const stars: ReactElement[] = [];
+    const stars: JSX.Element[] = [];
     for (let i = 0; i < fullStars; i++) stars.push(<span key={`full-${i}`} className="material-icons profile-recent-review-star">star</span>);
     if (hasHalf) stars.push(<span key="half" className="material-icons profile-recent-review-star">star_half</span>);
     for (let i = 0; i < emptyStars; i++) stars.push(<span key={`empty-${i}`} className="material-icons profile-recent-review-star profile-recent-review-star-empty">star_border</span>);
